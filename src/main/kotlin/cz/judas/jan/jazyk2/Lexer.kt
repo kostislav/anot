c2588a28dba8466abc925963392e8cd55c74f276
Jan Judas <jan@judas.cz>
2024-01-05 20:28:06 +0100

    lexer for Hello world

---
+++ src/main/kotlin/cz/judas/jan/jazyk2/Lexer.kt
@@ -0,0 +1,78 @@
+package cz.judas.jan.jazyk2
+
+import java.io.InputStream
+
+class Lexer {
+    private val symbols = setOf('/', '(', ')', '@', ':')
+
+    fun parseTokens(input: InputStream): List<Token> {
+        return input.reader().readLines()
+            .flatMapIndexed(::parseLine)
+    }
+
+    private fun parseLine(lineNumber: Int, line: String): List<Token> {
+        if (line.isEmpty()) {
+            return listOf(Token.EmptyLine)
+        } else {
+            val tokens = mutableListOf<Token>()
+            var pos = 0
+
+            while (pos < line.length) {
+                if (isSymbol(line[pos])) {
+                    val start = pos
+                    while (pos < line.length && isSymbol(line[pos])) {
+                        pos++
+                    }
+                    tokens += Token.Symbol(line.substring(start, pos))
+                } else if (line[pos] == ' ') {
+                    val start = pos
+                    while (pos < line.length && line[pos] == ' ') {
+                        pos++
+                    }
+                    tokens += Token.Whitespace(pos - start)
+                } else if (isAlphanumeric(line[pos])) {
+                    val start = pos
+                    while (pos < line.length && isAlphanumeric(line[pos])) {
+                        pos++
+                    }
+                    tokens += Token.Alphanumeric(line.substring(start, pos))
+                } else if (line[pos] == '\'') {
+                    pos++
+                    val start = pos
+                    // TODO escape sequences
+                    while (line[pos] != '\'') {
+                        pos++
+                        if (pos == line.length) {
+                            throw RuntimeException("Unterminated string literal on line ${lineNumber + 1} starting at position ${start}")
+                        }
+                    }
+                    tokens += Token.StringValue(line.substring(start, pos))
+                    pos++
+                } else {
+                    throw RuntimeException("Unexpected character ${line[pos]} on line ${lineNumber + 1} at position ${pos + 1}")
+                }
+            }
+
+            tokens += Token.Newline
+
+            return tokens
+        }
+    }
+
+    private fun isAlphanumeric(c: Char): Boolean {
+        return c in 'a'..'z' || c in 'A'..'Z' || c in '0'..'9'
+    }
+
+    private fun isSymbol(c: Char): Boolean {
+        return c in symbols
+    }
+}
+
+sealed interface Token {
+    data class Whitespace(val amount: Int) : Token
+    data object Newline: Token
+    data object EmptyLine: Token
+    data class Alphanumeric(val value: String): Token
+    data class Symbol(val value: String): Token
+    data class StringValue(val value: String): Token
+}
---
+++ src/test/kotlin/cz/judas/jan/jazyk2/LexerTest.kt
@@ -0,0 +1,64 @@
+package cz.judas.jan.jazyk2
+
+import cz.judas.jan.jazyk2.Token.Alphanumeric
+import cz.judas.jan.jazyk2.Token.EmptyLine
+import cz.judas.jan.jazyk2.Token.Newline
+import cz.judas.jan.jazyk2.Token.StringValue
+import cz.judas.jan.jazyk2.Token.Symbol
+import cz.judas.jan.jazyk2.Token.Whitespace
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.InputStream
+import java.lang.RuntimeException
+import kotlin.test.assertEquals
+
+class LexerTest {
+    private lateinit var lexer: Lexer
+
+    @BeforeEach
+    fun setUp() {
+        lexer = Lexer()
+    }
+
+    @Test
+    fun parsesHelloWorld() {
+        val input = inputStream(
+            """
+            import /stdlib/io/println
+
+            @entrypoint
+            def main():
+                println('Hello, world')
+        """.trimIndent()
+        )
+
+        val parsedLines = lexer.parseTokens(input)
+
+        assertThat(
+            parsedLines, equalTo(
+                listOf(
+                    Alphanumeric("import"), Whitespace(1), Symbol("/"), Alphanumeric("stdlib"), Symbol("/"), Alphanumeric("io"), Symbol("/"), Alphanumeric("println"), Newline,
+                    EmptyLine,
+                    Symbol("@"), Alphanumeric("entrypoint"), Newline,
+                    Alphanumeric("def"), Whitespace(1), Alphanumeric("main"), Symbol("():"), Newline,
+                    Whitespace(4), Alphanumeric("println"), Symbol("("), StringValue("Hello, world"), Symbol(")"), Newline
+                )
+            )
+        )
+    }
+
+    @Test
+    fun failsForInvalidInput() {
+        val input = inputStream("import ^eh")
+
+        val exception = assertThrows<RuntimeException> { lexer.parseTokens(input) }
+        assertEquals(exception.message, "Unexpected character ^ on line 1 at position 8")
+    }
+
+    private fun inputStream(content: String): InputStream {
+        return content.byteInputStream()
+    }
+}
